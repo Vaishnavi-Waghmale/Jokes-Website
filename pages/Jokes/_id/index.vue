e535641e9985fa6b214427a66028f2d4f75bd4d0
Vaishnavi-Waghmale <vaishnaviwaghmale@gmail.com>
2022-11-02 21:57:48 +0530

    new file:   pages/Jokes/_id/index.vue 	deleted:    components/NuxtLogo.vue 	deleted:    components/Tutorial.vue 	modified:   pages/about.vue 	modified:   pages/index.vue

---
+++ pages/Jokes/_id/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        {{ $route.params.id }}
+    </div>
+</template>
+
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            joke: {}
+        }
+    },
+
+    async created() {
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.get(`https://icanhazdadjoke.com/`, config);
+
+            this.jokes = res.data.results;
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+}
+
+</script>
